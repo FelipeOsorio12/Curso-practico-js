8f61114f1e9fa138ec374b33277ebc913386831f
felipe osorio <felipe.osorio12@utp.edu.co>
2024-06-02 19:53:04 -0500

    escuchar eventos

--- script.js
+++ script.js
@@ -1,50 +1,16 @@
-/*
-    La forma que tenemos de interactuar con un documento html desde nuestro archivo de js, es mediante el objeto "document", esto es posible gracias al navegador que utilizemos el cual nos brinda toda estas funciones, pero en otros entornos como node.js para el manejo de backend no existen este tipo de funciones 
-*/
+//Escuhar eventos
 
-/*
-    Ya para traer propiamente un elemento del html al archivo js hay varias formas, la siguiente es una de estas:
-*/
-
-const h1 = document.querySelector('h1') //no esta trayendo todos los h1 del documento html, trae el primero que encuentra.
-const p = document.querySelector('p') 
-const parrafito = document.querySelector('.parrafito') //acá esta haciendo referencia a la clase "parrafito"
-const pid =  document.querySelector('#pid') // acá esta haciendo referencia al id "pid"
-const input =   document.querySelector('input')
+const h1 = document.querySelector('h1') 
+const input1 = document.querySelector('#calculo1')
+const input2 = document.querySelector('#calculo2')
+const btn = document.querySelector('#btncalcular')
+const result = document.querySelector('#result')
 
 
-// Como es de forma habitual utilizamos el console.log para depurar nuestro codigo y mostrar lo que contiene las constantes que definimos anteriormente 
-console.log({ 
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-})
+btn.addEventListener('click', btnOnclick)
 
-// Para poder insertar texto o nuevas etiquetas dentro del documento html a traves del archivo .js, lo hacemos mediante el metodo .innerHTML
-h1.innerHTML= 'patito' 
+function btnOnclick (){
 
-/*
-
-// El metodo .getAttribute nos trae el atributo establecido de algún elemento del documento HTML
-h1.getAttribute('class')
-// Y con el metodo de .setAttribute podemos cambiar el atributo establecido de algún elemento del documento HTML
-h1.setAttribute('class','rojo')
-
-*/
-
-//Si lo que se quiere modificar es una clase en especifico, lo hacemos de la siguiente manera:
-
-
-h1.classList.add('rojo') //acá le estamos asignando la clase rojo a la etiqueta h1
-h1.classList.remove('verde')//Y acá le estamos quitando la clase verde a la etiqueta h1
-
-//Para crear el elemento de html que nosotros queramos desde JS, usamos el siguiente metodo:
-
-const img = document.createElement('img'); //Creando un elemento/etiqueta de tipo imagen de html desde js
-
-img.setAttribute('src', 'Assets/imagen1.png') //Al anterior elemento/etiqueta que creamos le estamos asignado el atributo "src" que tiene toda imagen
-
-pid.innerHTML=''
-pid.append(img) // a nuestra constante que tiene el elemento/etiqueta con id "pid" le estamnos agregando el elemento img que creamos desde JS
+    let suma= parseInt(input1.value) + parseInt(input2.value)
+    result.innerHTML = "Resultado: " + suma
+}
